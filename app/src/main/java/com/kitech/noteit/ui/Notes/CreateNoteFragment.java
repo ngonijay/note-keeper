12d0dede6f43161570903391d19cbd29984c4fd7
Ngonidzashe.Jambo <R0pafadz0@80>
2022-11-01 16:36:07 +0200

    add: view notes ui

--- app/src/main/java/com/kitech/noteit/ui/Notes/CreateNoteFragment.java
+++ app/src/main/java/com/kitech/noteit/ui/Notes/CreateNoteFragment.java
@@ -2,6 +2,8 @@ package com.kitech.noteit.ui.Notes;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +11,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -18,6 +19,7 @@ import com.kitech.noteit.R;
 import com.kitech.noteit.databinding.FragmentCreateNoteBinding;
 import com.kitech.noteit.domain.NoteEntity;
 import com.kitech.noteit.ui.Notes.viewmodels.CreateNoteViewModel;
+import com.kitech.noteit.utils.CONSTANTS;
 import com.kitech.noteit.utils.CustomDateTimeMethods;
 
 import java.util.Objects;
@@ -26,6 +28,7 @@ public class CreateNoteFragment extends Fragment {
 
     private CreateNoteViewModel mViewModel;
     private FragmentCreateNoteBinding mBinding;
+    private long mNoteId;
 
     public static CreateNoteFragment newInstance() {
         return new CreateNoteFragment();
@@ -41,52 +44,68 @@ public class CreateNoteFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        if (getArguments() != null) {
+            mNoteId = getArguments().getLong(CONSTANTS.NOTE_ID);
+        }
         initializeView();
         mBinding.noteCreatedTimeTxt.setText(CustomDateTimeMethods.getCurrentLocalDateTime());
     }
 
-    private void saveNoteInDb() {
+    private void processNote() {
+
+        if (mNoteId != 0)
+            updateNote(mNoteId);
+        else
+            saveNoteIn();
+    }
+
+    private void saveNoteIn() {
         NoteEntity noteEntity = new NoteEntity();
-            noteEntity.setNoteTitle(Objects.requireNonNull(mBinding.noteTitleTextEditText.getText()).toString());
-            noteEntity.setNoteMessage(Objects.requireNonNull(mBinding.noteMessageTextEditText.getText()).toString());
-            noteEntity.setNoteCreatedDate(Objects.requireNonNull(mBinding.noteCreatedTimeTxt.getText()).toString());
-            noteEntity.setNoteModifiedDate("");
+        noteEntity.setNoteTitle(Objects.requireNonNull(mBinding.noteTitleTextEditText.getText()).toString());
+        noteEntity.setNoteMessage(Objects.requireNonNull(mBinding.noteMessageTextEditText.getText()).toString());
+        noteEntity.setNoteCreatedDate(Objects.requireNonNull(mBinding.noteCreatedTimeTxt.getText()).toString());
+        noteEntity.setNoteModifiedDate("");
         mViewModel.saveNote(noteEntity);
 
         Toast.makeText(getContext(), "SAVED", Toast.LENGTH_SHORT).show();
         getActivity().onBackPressed();
-
     }
 
     private void initializeView() {
 
-        if( getArguments() != null){
-        long id = getArguments().getLong("NOTE_ID");
-        if (id != 0)
-            getNoteDetails(id);
+        if (mNoteId != 0)
+            prepopulateNoteFields(mNoteId);
 
-            mBinding.saveButton.setOnClickListener( v -> updateNote(id));
-            mBinding.saveButton.setText("UPDATE");
-        } else {
-            mBinding.saveButton.setText("SAVE");
-            mBinding.saveButton.setOnClickListener(v -> saveNoteInDb());
-        }
+        mBinding.toolbar.generalToolbar.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.menu_main, menu);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.action_save_note)
+                    processNote();
+                return false;
+            }
+        });
 
     }
 
     private void updateNote(long id) {
         mViewModel.getNoteById(id).observe(getViewLifecycleOwner(), note -> {
-            note.setNoteTitle(mBinding.noteTitleTextEditText.getText().toString());
-            note.setNoteCreatedDate(mBinding.noteCreatedTimeTxt.getText().toString());
-            note.setNoteMessage(mBinding.noteMessageTextEditText.getText().toString());
-
-            mViewModel.updateNote(note);
-            getActivity().onBackPressed();
+            if (note != null) {
+                note.setNoteTitle(mBinding.noteTitleTextEditText.getText().toString());
+                note.setNoteCreatedDate(mBinding.noteCreatedTimeTxt.getText().toString());
+                note.setNoteMessage(mBinding.noteMessageTextEditText.getText().toString());
+                mViewModel.updateNote(note);
+                getActivity().onBackPressed();
+            }
         });
     }
 
-    private void getNoteDetails(long id) {
-        mViewModel.getNoteById(id).observe(getViewLifecycleOwner(), note ->{
+    private void prepopulateNoteFields(long id) {
+        mViewModel.getNoteById(id).observe(getViewLifecycleOwner(), note -> {
             mBinding.noteTitleTextEditText.setText(note.getNoteTitle());
             mBinding.noteCreatedTimeTxt.setText(note.getNoteCreatedDate());
             mBinding.noteMessageTextEditText.setText(note.getNoteMessage());
---
+++ app/src/main/java/com/kitech/noteit/utils/CONSTANTS.java
@@ -0,0 +1,6 @@
+package com.kitech.noteit.utils;
+
+public interface CONSTANTS {
+    String NOTE_ID = "NOTE_ID";
+
+}
